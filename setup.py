4a3d04f51e25309baca240f3b66f34c32509563d
marvin <knollmarvin6@gmail.com>
2022-07-18 16:42:19 +0200

    feat: add license

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     maintainer="marvin",
     maintainer_email="marvin.knoll@protonmail.com",
     description="TODO: Package description",
-    license="TODO: License declaration",
+    license="Apache-2.0",
     tests_require=["pytest"],
     entry_points={
         "console_scripts": [
